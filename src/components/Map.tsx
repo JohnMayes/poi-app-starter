d94c1a96a431a2bd8bcf27ee7e9a770b2bb7f655
JohnMayes <36653554+JohnMayes@users.noreply.github.com>
2022-03-31 16:59:06 -0500

    init react-map-gl

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,7 @@
 // import daMap from './daMap.png';
 import { placeArrType } from './../App';
+import ReactMapGL from 'react-map-gl';
+import { useState } from 'react';
 
 interface IMapProps {
   places: placeArrType;
@@ -7,24 +9,35 @@ interface IMapProps {
 }
 
 function Map(props: IMapProps) {
+  const [viewBox, setViewBox] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    width: 600,
+    height: 400,
+    zoom: 14,
+  });
+
   return (
-    <section className="Comp-map">
-      {/* <figure>
-        <img src={daMap} alt="its da Map" />
-        <figcaption>Its da Map!</figcaption>
-      </figure> */}
-      <h1>Places:</h1>
-      <hr></hr>
-      <div className="placeButtons">
-        {props.places.map((i) => {
-          return (
-            <button type="button" onClick={() => props.loadPlaceToEdit(i.key)}>
-              {i.name.toString()}
-            </button>
-          );
-        })}
-      </div>
-    </section>
+    <div>
+      <ReactMapGL
+        {...viewBox}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      >
+        anything here?
+      </ReactMapGL>
+    </div>
+    // <section className="Comp-map">
+    //   <h1>Places:</h1>
+    //   <div className="placeButtons">
+    //     {props.places.map((i) => {
+    //       return (
+    //         <button type="button" onClick={() => props.loadPlaceToEdit(i.key)}>
+    //           {i.name.toString()}
+    //         </button>
+    //       );
+    //     })}
+    //   </div>
+    // </section>
   );
 }
 
