fa12f19bbdf77ae8ac5fe916f42ec8edd68962cc
JohnMayes <36653554+JohnMayes@users.noreply.github.com>
2022-04-01 15:34:54 -0500

    untiall react-map-gl

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,23 @@
-// import daMap from './daMap.png';
 import { placeArrType } from './../App';
 import mapboxgl from 'mapbox-gl';
-import React, { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface IMapProps {
   places: placeArrType;
   loadPlaceToEdit: (key: string) => void;
 }
 
-interface RefObject<T> {
-  readonly current: T | null;
-}
+const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string;
 
-const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoiam1heWVzbWFwYm94IiwiYSI6ImNsMWNuOHFvODA1NmUza25zc29hMDc2NmMifQ.u23fMCsFVGbpYGon-Z1_Bw';
+mapboxgl.accessToken = accessToken;
 
 function Map(props: IMapProps) {
   const mapContainerRef = useRef<HTMLDivElement>(null);
 
-  // initialize map when component mounts
+  // BEGIN USE EFFECT: initialize map when component mounts
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current as string | HTMLElement,
-      // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-104.9876, 39.7405],
       zoom: 12.5,
@@ -35,7 +28,7 @@ function Map(props: IMapProps) {
 
     // clean up on unmount
     return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // END USE EFFECT
 
   return (
     <div className="map-container" ref={mapContainerRef} />
