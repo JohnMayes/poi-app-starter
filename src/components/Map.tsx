4f618293bb8423f30fb77a92577c3a708b0edbec
JohnMayes <36653554+JohnMayes@users.noreply.github.com>
2022-03-31 19:46:36 -0500

    removed react-map-gl

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,31 +1,44 @@
 // import daMap from './daMap.png';
 import { placeArrType } from './../App';
-import ReactMapGL from 'react-map-gl';
-import { useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface IMapProps {
   places: placeArrType;
   loadPlaceToEdit: (key: string) => void;
 }
 
+interface RefObject<T> {
+  readonly current: T | null;
+}
+
+const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoiam1heWVzbWFwYm94IiwiYSI6ImNsMWNuOHFvODA1NmUza25zc29hMDc2NmMifQ.u23fMCsFVGbpYGon-Z1_Bw';
+
 function Map(props: IMapProps) {
-  const [viewBox, setViewBox] = useState({
-    longitude: -122.4,
-    latitude: 37.8,
-    width: 600,
-    height: 400,
-    zoom: 14,
-  });
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  // initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current as string | HTMLElement,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-104.9876, 39.7405],
+      zoom: 12.5,
+    });
+
+    // add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    // clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div>
-      <ReactMapGL
-        {...viewBox}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-      >
-        anything here?
-      </ReactMapGL>
-    </div>
+    <div className="map-container" ref={mapContainerRef} />
     // <section className="Comp-map">
     //   <h1>Places:</h1>
     //   <div className="placeButtons">
