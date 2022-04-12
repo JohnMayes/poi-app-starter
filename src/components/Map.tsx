c20710155cc910c1ac91b1f5271a456e0021e694
JohnMayes <36653554+JohnMayes@users.noreply.github.com>
2022-04-12 13:34:05 -0500

    init controlled map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import { placeArrType } from './../App';
-import { useEffect, useRef } from 'react';
-import Map, { GeolocateControl, Marker, NavigationControl } from 'react-map-gl';
+import { useRef, useCallback, useState } from 'react';
+import MapGL, { GeolocateControl, NavigationControl } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 interface IMapProps {
@@ -12,25 +12,47 @@ const MAPBOX_TOKEN =
   'pk.eyJ1Ijoiam1heWVzbWFwYm94IiwiYSI6ImNsMWNuOHFvODA1NmUza25zc29hMDc2NmMifQ.u23fMCsFVGbpYGon-Z1_Bw';
 
 function DisplayMap(props: IMapProps) {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef(null);
+  const geocoderContainerRef = useRef(null);
+
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides,
+      });
+    },
+    [handleViewportChange]
+  );
 
   return (
     <div className="map-container" ref={mapContainerRef}>
-      <Map
-        initialViewState={{
-          latitude: 37.8,
-          longitude: -122.4,
-          zoom: 14,
-        }}
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        onMove={handleViewportChange}
         style={{ width: 800, height: 600 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         mapboxAccessToken={MAPBOX_TOKEN}
       >
-        <Marker longitude={-122.4} latitude={37.8} color="red" />
-
         <NavigationControl position="bottom-right" />
         <GeolocateControl />
-      </Map>
+      </MapGL>
     </div>
   );
 }
